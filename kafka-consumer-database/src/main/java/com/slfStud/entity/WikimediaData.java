f25e8ac39fd2d325a83c5eb7ace61d9714349c6e
alber-ayman <63537362+alber-ayman@users.noreply.github.com>
2025-08-06 15:43:52 +0300

    Add files via upload

---
+++ kafka-consumer-database/src/main/java/com/slfStud/entity/WikimediaData.java
@@ -0,0 +1,33 @@
+package com.slfStud.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Wikimedia_recentchange")
+public class WikimediaData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private String wikiEventData;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getWikiEventData() {
+        return wikiEventData;
+    }
+
+    public void setWikiEventData(String wikiEventData) {
+        this.wikiEventData = wikiEventData;
+    }
+}
